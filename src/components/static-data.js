b40f19a10c063459a2ede7aaa69de2cd9b0778f0
Jesse Oudom <jesse.oudom@yahoo.com>
2022-10-29 13:45:51 -0400

    adds completed shopper app

---
+++ src/components/static-data.js
@@ -0,0 +1,26 @@
+let items = [ 
+    { 
+        id: 0, 
+        name: "Apple iPad", 
+        description: "An iPad like no other.", 
+        price: 329.00,
+        pic: <img 
+            src="https://i.pcmag.com/imagery/reviews/027VnjtmbHOqkzUlbLQmyB4-2.fit_lim.size_1050x591.v1648641055.jpg" 
+            alt="ipad"
+            height="64px" 
+            width="64px"/>
+    }, 
+    { 
+        id: 1, 
+        name: "Apple iPad Pro", 
+        description: "Even more expensive than the iPad.", 
+        price: 729.00,
+        pic: <img 
+            src="https://cdn.pocket-lint.com/r/s/970x/assets/images/151545-tablets-review-apple-ipad-pro-129-inch-2020-review-business-as-usual-image1-buq5ipfo1e.jpg" 
+            alt="ipad-pro"
+            height="64px" 
+            width="64px"/> 
+    }, 
+]; 
+    
+export {items}; 
